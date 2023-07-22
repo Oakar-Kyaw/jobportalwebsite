182e29407dbc35e1a8405ccd50cd537f852ae0d7
Oakar Kyaw <107309612+Oakar-Kyaw@users.noreply.github.com>
2023-07-22 22:07:38 +0630

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(bodyparser.urlencoded({
 
 
 app.use(cookieParser());
-app.use(cors({
+app.use('*',cors({
     origin:'http://localhost:3000',          
     optionSuccessStatus:200,
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
