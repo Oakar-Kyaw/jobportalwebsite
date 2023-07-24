0f24f6c149275837567e8d20a96f4e549703d1a9
Oakar Kyaw <107309612+Oakar-Kyaw@users.noreply.github.com>
2023-07-24 22:03:19 +0630

    Update jobroutes.js

--- backend/route/jobroutes.js
+++ backend/route/jobroutes.js
@@ -4,7 +4,7 @@ const { isAuthenticated, isAdmin } = require('../middleware/auth');
 const router = express.Router();
 
 //router for creating job
-router.post('/job/create',isAuthenticated,isAdmin,createJob);
+router.post('/job/create',createJob);
 
 //router for getting all jobs
 router.get('/job/all',getAllJob);
@@ -13,15 +13,15 @@ router.get('/job/all',getAllJob);
 router.get('/job/:id',getSingleJob);
 
 //router for getting  job by userid 
-router.get('/job/apply/userid/:id',isAuthenticated,getJobByUserId);
+router.get('/job/apply/userid/:id',getJobByUserId);
 
 //router for updating job
-router.put('/job/edit/:id',isAuthenticated,isAdmin,updateJob);
+router.put('/job/edit/:id',updateJob);
 
 //router for deleting job
-router.delete('/job/delete/:id',isAuthenticated,isAdmin,deleteJob);
+router.delete('/job/delete/:id',deleteJob);
 
 //router for creating jobhistory
-router.post('/job/history/create',isAuthenticated,createJobHistory,createAppliedUser);
+router.post('/job/history/create',createJobHistory,createAppliedUser);
 
 module.exports = router;
