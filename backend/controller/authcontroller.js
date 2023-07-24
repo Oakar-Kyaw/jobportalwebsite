50a7e3f26a0121f7eb07e2789027ae4a6655d979
Oakar Kyaw <107309612+Oakar-Kyaw@users.noreply.github.com>
2023-07-24 13:27:07 +0630

    Update authcontroller.js

--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -54,9 +54,8 @@ exports.signin= async function(req,res,next){
 const sendJsonWebToken=async function(user,statusCode,res){
    let token= await user.getjsontoken();
    console.log("cookie is" );
-   res
-   .cookie('token',token,{maxAge:4*60*60*1000})
-   .json({success:true,token,user});
+   res.cookie('token',token,{maxAge:4*60*60*1000});
+   res.json({success:true,token,user});
    console.log("pass cookie is" );
 }
 
